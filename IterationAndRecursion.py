dc007fdfdbf6c3fa4abce0582185301106d788e8
username <useremail>
2023-04-19 12:39:54 -0700

    gvvkhg

--- IterationAndRecursion.py
+++ IterationAndRecursion.py
@@ -1,26 +1,26 @@
-def get_factorial_for_loop(n): 
+print("fractorial result using the iterative function")
+
+def factorial_iterative(n):
     result = 1
+    for i in range(n):
+        result *= i + 1
+    return result
+def factorial_recursive(n)
+    if n = 1:
+        return 1
+    else:
+        return n * factorial_recursive(n)
+
+def get_factorial_for_loop(n):
+    result = 1 
     if n > 1:
-        for i in range (1, n+1):
+        for i in range(1, n+1): 
             result = result * i 
         return result
     else:
         return 'n has to be positive'
-inp = input("enter a whole number:  ")
+inp = input("enter a number:  ")
 inp = int(inp)
+print(f"the result is: {get_factorial_for_loop(inp)}"
 
-print(f"the result is:{get_factorial_for_loop(inp)}")
 
-def recur_factorial(n): 
-    if n == 1: 
-        return n 
-    else:
-        return n*recur_factorial(n-1)
-
-num = input("please choose a number:  ")
-if int(num) < 0: 
-    print("factorial does not exist for negitive numbers")
-elif num == 0:
-    print("thhe factorial of 0 is 1")
-else:
-    print("the factorial of", num, "is", recur_factorial(num))
