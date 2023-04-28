c81115c0506c992b361c5ba8f9d424df2f15d0f1
waall <waall@LAPTOP>
2023-04-27 20:00:51 -0500

    jfcfgj

--- IterationAndRecursion.py
+++ IterationAndRecursion.py
@@ -1,27 +1,24 @@
-from math import factorial
-
-print("fractorial result using the iterative function")
-
-n = int(input("enter a number:  "))
-
-factorial = 1
-
-if n < 0:    
-    print("factors do not exist for negitive numbers.")
-elif n == 0:
-    print("the factorial of 0 is 1")
-else:
-    for i in range(1,n + 1):
-        factorial = factorial*i
-    print("the factorial of ",n," is ", factorial)
-for i in range(n):
-    print(i)
-
-def factorial(n):
-    if n == 1:
-        return 1 
+#whitnie allison cis 261 iteration and recursion
+def factorial_recursive(num):
+    if num == 0 or num == 1:
+        return 1
     else:
-        return (n * factorial(n-1))
+        return num * factorial_recursive(num - 1)
 
-num = 3 
-print("the factorial of ", num, " is ", factorial(num))
+def factorial_iterative(num):
+    fact = 1
+    for number in range(2, num+1):
+        fact = number * fact
+    return fact
+ 
+def main():
+    numList = [0, 5, 10, 25, 50, 100]
+    print ("Factorial results using an iterative function")
+    for num in numList:
+        print(f'{num}! = {factorial_iterative(num)}')
+    print ("Factorial results using a recursive function")
+    for num in numList:
+        print(num, '! = ', factorial_recursive(num))
+
+if __name__ == "__main__":
+    main()
